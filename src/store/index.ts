aa535c71470de2c5123524769bbfc4cf7700c515
Natalia <icxodnik@gmail.com>
2024-05-12 15:15:06 +0200

    fix linter errors

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,4 +1,4 @@
-import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
 import phoneBookReducer from './phoneBook.slice';
 
 import { persistStore, persistReducer } from 'redux-persist';
